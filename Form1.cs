101a0d29b11180154888cb7c4deca78a54773083
黄文 <mychenxing_net@sina.cn>
2017-10-20 21:15:22 +0800

    添加项目文件。

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,5 @@ namespace LCApp {
         public Form1() {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e) {
-
-        }
     }
 }
